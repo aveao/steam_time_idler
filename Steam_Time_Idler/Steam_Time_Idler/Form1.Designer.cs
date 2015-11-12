745a18fdaefffbc2304285d2c3c74ce6cf266c1a
ardaozkal <ardaozkal@live.com>
2015-11-13 00:24:46 +0200

    Release

---
+++ Steam_Time_Idler/Steam_Time_Idler/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Steam_Time_Idler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(106, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Game List";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(106, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Your Steam Link:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(106, 71);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(133, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Play games for:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(245, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "minutes";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(106, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(206, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Start Idling";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(294, 16);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(18, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "?";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(294, 68);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(18, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "?";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Status: Nothing.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 155);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Steam Time Idler";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Steam_Time_Idler/Steam_Time_Idler/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Security.Principal;
+using System.Diagnostics;
+using System.Management;
+
+namespace Steam_Time_Idler
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<long, decimal> games = new Dictionary<long, decimal>();
+        public Form1()
+        {
+            InitializeComponent();
+            stopidle();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Two sample links: " + Environment.NewLine + "http://steamcommunity.com/id/ardaozkal" + Environment.NewLine + "http://steamcommunity.com/profiles/76561198034299068");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("5 minutes lets you review the game and is therefore enough for most people.");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label4.Text = "Status: please wait, this will take some time";
+            MessageBox.Show("This will take some time if you have a lot of games (~1 minute on slow internet + slow pc + 1100 games) and it will start after you click OK.");
+            if (textBox1.Text == "me")
+            {
+                textBox1.Text = "http://steamcommunity.com/id/ardaozkal";
+            }
+
+            var link = (textBox1.Text.Substring(textBox1.Text.Length - 1) != "/") ? textBox1.Text + "/" : textBox1.Text; //get the link
+            var wc = new WebClient();
+            var returnedlines = wc.DownloadString(link + "games?tab=all&xml=1").Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+            wc.Dispose();
+
+            /*
+            <game>
+			<appID>252950</appID>
+			<name><![CDATA[Rocket League]]></name>
+			<logo><![CDATA[http://cdn.akamai.steamstatic.com/steamcommunity/public/images/apps/252950/58d7334290672887fdd47e25251f291b812c895e.jpg]]></logo>
+			<storeLink><![CDATA[http://steamcommunity.com/app/252950]]></storeLink>
+			<hoursLast2Weeks>5.3</hoursLast2Weeks>
+			<hoursOnRecord>5.3</hoursOnRecord>
+			<statsLink><![CDATA[http://steamcommunity.com/id/ardaozkal/stats/252950]]></statsLink>
+			<globalStatsLink><![CDATA[http://steamcommunity.com/stats/252950/achievements/]]></globalStatsLink>
+		    </game>
+            */
+            long currentappid = 0;
+            var currentline = 0;
+
+            foreach (string line in returnedlines)
+            {
+                currentline++;
+                if (line.Contains("<appID>"))
+                {
+                    currentappid = int.Parse(line.Replace(" ", "").Replace("\\t", "").Replace("<appID>", "").Replace("</appID>", ""));
+                    if (returnedlines.Count() > currentline + 4)
+                    {
+                        var plusfour = returnedlines[currentline + 4];
+                        if (plusfour.Contains("<statsLink>"))
+                        {
+                            plusfour = returnedlines[currentline + 3];
+                        }
+                        if (!plusfour.Contains("<hoursOnRecord>"))
+                        {
+                            games.Add(currentappid, 0);
+                        }
+                    }
+                    else
+                    {
+                        games.Add(currentappid, 0);
+                    }
+                }
+                else if (line.Contains("<hoursOnRecord>"))
+                {
+                    try
+                    {
+                        games.Add(currentappid, decimal.Parse(line.Replace(" ", "").Replace("\\t", "").Replace("<hoursOnRecord>", "").Replace("</hoursOnRecord>", "")));
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                }
+            }
+
+            button2.Enabled = true;
+
+            MessageBox.Show("Ready to start idling, hit start idle button please.");
+            label4.Text = "Status: Ready to idle.";
+        }
+
+        List<long> gamestoidle = new List<long>();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label4.Text = "Status: Starting idling.";
+            foreach (long game in games.Keys)
+            {
+                decimal timeplayed = 0;
+                games.TryGetValue(game, out timeplayed);
+                if (timeplayed < numericUpDown1.Value / 50) // value/50 returns the time out of 1 and not 60, for ex, 5 minutes is 0.1 hours, which is what steam returns on xml results
+                {
+                    gamestoidle.Add(game);
+                }
+            }
+            timer1.Interval = 100; //starts first run immediately
+            timer1.Start();
+
+            numericUpDown1.Enabled = false;
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+            button2.Enabled = false;
+            label4.Text = "Status: Started idling.";
+        }
+
+        static void stopidle()
+        {
+            try
+            {
+                var username = WindowsIdentity.GetCurrent().Name;
+                foreach (var process in Process.GetProcessesByName("steam-idle"))
+                {
+                    var searcher = new ManagementObjectSearcher("Select * From Win32_Process Where ProcessID = " + process.Id);
+                    var processList = searcher.Get();
+
+                    foreach (ManagementObject obj in processList)
+                    {
+                        var argList = new string[] { string.Empty, string.Empty };
+                        var returnVal = Convert.ToInt32(obj.InvokeMethod("GetOwner", argList));
+                        if (returnVal == 0)
+                        {
+                            if (argList[1] + "\\" + argList[0] == username)
+                            {
+                                process.Kill();
+                            }
+                        }
+                    }
+                    searcher.Dispose();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error while stopping idle, please shut down all steam-idle.exe processes by hand from task manager.");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = Convert.ToInt32(numericUpDown1.Value * 60000); //sets the time of the future ones to longer time intervals than 100msec
+            var maxidle = 20; //I contacted jshackles and asked him about the limit (no answers yet) but yeah I'm hoping 20 is ok.
+
+            if (gamestoidle.Count == 0)
+            {
+                timer1.Stop();
+                numericUpDown1.Enabled = true;
+                button1.Enabled = true;
+                textBox1.Enabled = true;
+
+                label4.Text = "Status: Done idling.";
+                MessageBox.Show("Done");
+            }
+            else
+            {
+                stopidle(); //not the best way as it can interfere with idle master, but why does one run both at the same time?
+                if (gamestoidle.Count < maxidle) //so yeah, running 30 processes when there is 29 games to idle will cause a crash
+                {
+                    maxidle = gamestoidle.Count; //lets ste the max idle limit to the available game amount to prevent crashes
+                }
+
+                for (int i = 0; i <= maxidle; i++)
+                {
+                    Process.Start(new ProcessStartInfo("steam-idle.exe", gamestoidle[0].ToString()) { WindowStyle = ProcessWindowStyle.Hidden }); //this line was GLORIOUSLY ~~stolen~~ borrowed from idle master. Thanks jshackles.
+                    gamestoidle.Remove(gamestoidle[0]); //might be problematic sometime but who cares atm lol, will look more into this if this ever gets filed as an issue. Also tip for future self: use i instead of 0 on top and remove the idle game AFTER you play it.
+                }
+                label4.Text = "Status: Idling, " + gamestoidle.Count + " games left.";
+            }
+        }
+    }
+}
