ead3e9e0cf313fdfdb4fb79af45e6dedf406a9eb
ardaozkal <ardaozkal@live.com>
2015-11-21 19:36:46 +0200

    v1.2
    Hidden games option
    
    stopping idling option
    
    bugfixes
    
    it now checks the link you provided

--- Steam_Time_Idler/Steam_Time_Idler/Form1.Designer.cs
+++ Steam_Time_Idler/Steam_Time_Idler/Form1.Designer.cs
@@ -40,14 +40,16 @@
             this.button4 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(106, 42);
+            this.button1.Location = new System.Drawing.Point(16, 42);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(206, 23);
+            this.button1.Size = new System.Drawing.Size(296, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Get Game List";
             this.button1.UseVisualStyleBackColor = true;
@@ -117,9 +119,9 @@
             // button2
             // 
             this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(106, 97);
+            this.button2.Location = new System.Drawing.Point(152, 97);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(206, 23);
+            this.button2.Size = new System.Drawing.Size(160, 23);
             this.button2.TabIndex = 6;
             this.button2.Text = "Start Idling";
             this.button2.UseVisualStyleBackColor = true;
@@ -158,11 +160,33 @@
             this.label4.TabIndex = 9;
             this.label4.Text = "Status: Nothing.";
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(41, 97);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(105, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Idle hidden games";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(16, 97);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(19, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "?";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(326, 155);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -194,6 +218,8 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- Steam_Time_Idler/Steam_Time_Idler/Form1.cs
+++ Steam_Time_Idler/Steam_Time_Idler/Form1.cs
@@ -17,10 +17,29 @@ namespace Steam_Time_Idler
     public partial class Form1 : Form
     {
         Dictionary<long, decimal> games = new Dictionary<long, decimal>();
+        Dictionary<string, long> hiddengames = new Dictionary<string, long>();
+
         public Form1()
         {
             InitializeComponent();
             stopidle();
+
+            hiddengames.Add("Greenlight", 765);
+            hiddengames.Add("SteamVR", 250820);
+            hiddengames.Add("Steam Cloud", 764);
+            hiddengames.Add("Steam (Client)", 753);
+            hiddengames.Add("Steam Music", 258280);
+            hiddengames.Add("Steam Economy", 754);
+            hiddengames.Add("Steam Artwork", 767767);
+            hiddengames.Add("Steam Screenshots", 760);
+            hiddengames.Add("Steam Cloud - Videos", 761);
+            hiddengames.Add("Steam Cloud - User Logs", 764);
+            hiddengames.Add("ValveTestApp200269", 200269);
+            hiddengames.Add("Steam Controller Configs", 241100);
+            hiddengames.Add("Steam Software Beta Access", 218800);
+            hiddengames.Add("Game Library Sharing Access", 248210);
+            hiddengames.Add("Steam Trading Card Beta Access", 202352);
+            hiddengames.Add("Beta Access to the New Steam Community", 202351);
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -35,61 +54,69 @@ namespace Steam_Time_Idler
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label4.Text = "Status: Fetching game list.";
-            MessageBox.Show("This will take some time if you have a lot of games (~1 minute on slow internet + slow pc + 1100 games) and it will start after you click OK.");
             if (textBox1.Text == "me")
             {
                 textBox1.Text = "http://steamcommunity.com/id/ardaozkal";
             }
 
-            var link = (textBox1.Text.Substring(textBox1.Text.Length - 1) != "/") ? textBox1.Text + "/" : textBox1.Text; //get the link
-            var wc = new WebClient();
-            var returnedlines = wc.DownloadString(link + "games?tab=all&xml=1").Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
-            wc.Dispose();
-            
-            long currentappid = 0;
-            var currentline = 0;
-
-            foreach (string line in returnedlines) //TODO: XML PARSEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEER
+            if ((textBox1.Text.Contains("http://") || textBox1.Text.Contains("https://")) && (textBox1.Text.Contains("profiles") || textBox1.Text.Contains("id")))
             {
-                currentline++;
-                if (line.Contains("<appID>"))
+                label4.Text = "Status: Fetching game list.";
+                MessageBox.Show("This will take some time if you have a lot of games (~1 minute on slow internet + slow pc + 1100 games) and it will start after you click OK.");
+
+                var link = (textBox1.Text.Substring(textBox1.Text.Length - 1) != "/") ? textBox1.Text + "/" : textBox1.Text; //get the link
+                var wc = new WebClient();
+                var returnedlines = wc.DownloadString(link + "games?tab=all&xml=1").Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+                wc.Dispose();
+
+                long currentappid = 0;
+                var currentline = 0;
+
+                foreach (string line in returnedlines) //TODO: XML PARSEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEER
                 {
-                    currentappid = int.Parse(line.Replace(" ", "").Replace("\\t", "").Replace("<appID>", "").Replace("</appID>", ""));
-                    if (returnedlines.Count() > currentline + 4)
+                    currentline++;
+                    if (line.Contains("<appID>"))
                     {
-                        var plusfour = returnedlines[currentline + 4];
-                        if (plusfour.Contains("<statsLink>"))
+                        currentappid = int.Parse(line.Replace(" ", "").Replace("\\t", "").Replace("<appID>", "").Replace("</appID>", ""));
+                        if (returnedlines.Count() > currentline + 4)
                         {
-                            plusfour = returnedlines[currentline + 3];
+                            var plusfour = returnedlines[currentline + 4];
+                            if (plusfour.Contains("<statsLink>"))
+                            {
+                                plusfour = returnedlines[currentline + 3];
+                            }
+                            if (!plusfour.Contains("<hoursOnRecord>"))
+                            {
+                                games.Add(currentappid, 0);
+                            }
                         }
-                        if (!plusfour.Contains("<hoursOnRecord>"))
+                        else
                         {
                             games.Add(currentappid, 0);
                         }
                     }
-                    else
+                    else if (line.Contains("<hoursOnRecord>"))
                     {
-                        games.Add(currentappid, 0);
+                        try
+                        {
+                            games.Add(currentappid, decimal.Parse(line.Replace(" ", "").Replace("\\t", "").Replace("<hoursOnRecord>", "").Replace("</hoursOnRecord>", "")));
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                        }
                     }
                 }
-                else if (line.Contains("<hoursOnRecord>"))
-                {
-                    try
-                    {
-                        games.Add(currentappid, decimal.Parse(line.Replace(" ", "").Replace("\\t", "").Replace("<hoursOnRecord>", "").Replace("</hoursOnRecord>", "")));
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex);
-                    }
-                }
+
+                button2.Enabled = true;
+
+                MessageBox.Show("Ready to start idling, hit start idle button please.");
+                label4.Text = "Status: Ready to idle.";
             }
-
-            button2.Enabled = true;
-
-            MessageBox.Show("Ready to start idling, hit start idle button please.");
-            label4.Text = "Status: Ready to idle.";
+            else
+            {
+                MessageBox.Show("You don't seem to have a correct link. Example links can be accessed by pressing the ? button.");
+            }
         }
 
         List<long> gamestoidle = new List<long>();
@@ -113,6 +140,7 @@ namespace Steam_Time_Idler
             button1.Enabled = false;
             textBox1.Enabled = false;
             button2.Enabled = false;
+            button5.Text = "Stop Idling";
             label4.Text = "Status: Started idling.";
         }
 
@@ -155,9 +183,12 @@ namespace Steam_Time_Idler
             if (gamestoidle.Count == 0) //If we are done
             {
                 timer1.Stop(); //lets stop working shall we?
+                stopidle(); //Or you'll have loads of gametime.
                 numericUpDown1.Enabled = true; //lets reenable these
                 button1.Enabled = true;
                 textBox1.Enabled = true;
+                button2.Enabled = true;
+                button5.Text = "Idle hidden games";
 
                 label4.Text = "Status: Done idling."; //updating the status
                 MessageBox.Show("Done"); //And the users lived happily ever after. Thank me later.
@@ -177,6 +208,45 @@ namespace Steam_Time_Idler
                     //TODO: FIX THIS
                 }
                 label4.Text = "Status: Idling, " + gamestoidle.Count + " games left.";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var builder = new StringBuilder();
+            foreach (string gamename in hiddengames.Keys)
+            {
+                builder.Append(gamename + ", ");
+            }
+            MessageBox.Show("Clicking Idle Hidden Games button will play hidden games such as " + builder.ToString().Substring(0, (builder.Length - 2)) + ". You don't need to input your steam link if you are just planning to use this feature.");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.Text == "Stop Idling")
+            {
+                timer1.Stop();
+                stopidle();
+                numericUpDown1.Enabled = true; //lets reenable these
+                button1.Enabled = true;
+                textBox1.Enabled = true;
+                button2.Enabled = true;
+                label4.Text = "Status: Stopped idling.";
+                button5.Text = "Idle hidden games";
+            }
+            else
+            {
+                label4.Text = "Status: Idling hidden games. Hit the stop button to stop.";
+                numericUpDown1.Enabled = false;
+                button1.Enabled = false;
+                textBox1.Enabled = false;
+                button2.Enabled = false;
+                button5.Text = "Stop Idling";
+
+                foreach (long str in hiddengames.Values)
+                {
+                    Process.Start(new ProcessStartInfo("steam-idle.exe", str.ToString()) { WindowStyle = ProcessWindowStyle.Hidden }); //this line was borrowed from idle master. Thanks jshackles.
+                }
             }
         }
     }
